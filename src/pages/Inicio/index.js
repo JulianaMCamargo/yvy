e6be76bfbe94032353ab0e56f8bebf8920384fc3
Juliana Camargo <julianacamargopt@gmail.com>
2023-10-17 20:18:47 +0100

    Add path para os componentes e limpando o código

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,29 +1,20 @@
 import Banner from "components/Banner";
-import Descricao from "components/SecaoPadrao/Descricao";
-import Imagem from "components/SecaoPadrao/Imagem";
-import Secao from "components/SecaoPadrao/Secao";
-import Titulo from "components/SecaoPadrao/Titulo";
-import sobreNos from 'components/SecaoPadrao/Imagem/sobreNos2.jpeg';
 import Carrossel from "components/Carrossel/Carrossel";
+import Chamada from "components/Chamada";
+import ComoFunciona from "components/ComoFunciona";
+import Contato from "components/Contato/Contato";
+import SobreNos from "components/SobreNos";
 
 
-function Inicio(){
-    return(
+function Inicio() {
+    return (
         <>
             <Banner />
-            
-            <Secao corDeFundo='var(--branco)'>
-                <Titulo>Sobre Nós</Titulo>
-                <Imagem imagem={sobreNos}/>
-                <Descricao>Use esta página para compartilhar a missão, 
-                    visão ou história de sua empresa. 
-                    Diferencie-se da concorrência com uma 
-                    forte personalidade de marca e coloque 
-                    seus clientes em primeiro lugar.
-                </Descricao>
-            </Secao>
-            
+            <SobreNos />
             <Carrossel />
+            <Chamada />
+            <ComoFunciona />
+            <Contato />
         </>
     )
 }
