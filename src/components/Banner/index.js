ec2d6560c64f081e95287d9aac6ba993a852ba11
Juliana Camargo <julianacamargopt@gmail.com>
2023-10-17 20:19:10 +0100

    Add componente Botão

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,3 +1,4 @@
+import Botao from 'components/Botao';
 import styles from './Banner.module.css';
 import { Link } from 'react-router-dom';
 
@@ -5,11 +6,22 @@ function Banner() {
     return (
         <section className={styles.bannerImagem}>
             <div className={styles.bannerConteudo}>
-                <Link to='./'>YVYPORÃ</Link>
+                <Link to='./' className={styles.nomeEmpresa}>YVYPORÃ</Link>
+
                 <h1>O melhor lugar é aqui!</h1>
+
                 <div className={styles.botoes}>
-                    <button>Peça seu orçamento</button>
-                    <button>Contacte-nos</button>
+                    <Botao corDeFundo={'rgba(0,0,0,0.1)'} cor={'var(--branco)'}>
+                        <a href="#formulario" className={styles.link} style={{color: 'var(--branco)'}}>
+                            Pedir orçamento
+                        </a>
+                    </Botao>
+                    <Botao corDeFundo={'rgba(248, 251, 248, 0.8)'} cor={'var(--preto-medio)'} >
+                        <a href="#projetos" className={styles.link} style={{color: 'var(--preto-medio)'}}>
+                            Projetos
+                        </a>
+                    </Botao>
+
                 </div>
             </div>
         </section>
